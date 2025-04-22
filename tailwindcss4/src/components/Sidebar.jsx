dc42adc66f44cd97096ab20013ae4d3d7027af0b
Chaitra997 <lathapandla@gmail.com>
2025-04-22 22:22:29 +0530

    project is completed

--- tailwindcss4/src/components/Sidebar.jsx
+++ tailwindcss4/src/components/Sidebar.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import {
   MessageSquare,
@@ -16,7 +15,7 @@ const Sidebar = () => {
     location.pathname === path ? "bg-red-500" : "hover:bg-gray-100";
 
   return (
-    <aside className="h-full w-64 bg-white border-r border-gray-200 flex flex-col">
+    <aside className="hidden sm:block h-full w-64 bg-white border-r border-gray-200 flex flex-col">
       {/* Main Menu Section */}
       <div className="p-4">
         <h3 className="text-xs font-semibold text-gray-500 uppercase">Main Menu</h3>
@@ -51,13 +50,13 @@ const Sidebar = () => {
       <div className="p-4">
         <h3 className="text-xs font-semibold text-gray-500 uppercase">University</h3>
         <div className="mt-2 space-y-1">
-        <Link
-         to="/events"
-         className={`flex items-center gap-2 p-2 rounded-md ${getActiveClass("/events")}`}
-         >
-         <Calendar className="w-5 h-5" />
-         <span className="text-black">Events</span>
-       </Link>
+          <Link
+            to="/events"
+            className={`flex items-center gap-2 p-2 rounded-md ${getActiveClass("/events")}`}
+          >
+            <Calendar className="w-5 h-5" />
+            <span className="text-black">Events</span>
+          </Link>
 
           <Link
             to="/resources"
